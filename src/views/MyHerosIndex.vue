bcf66fee6097e647f4621454b07135786a1b3f47
Brian Hasal <brian.hasal@gmail.com>
2022-07-05 18:08:57 -0700

    Cleaned up relationships from backend views

--- src/views/MyHerosIndex.vue
+++ src/views/MyHerosIndex.vue
@@ -4,7 +4,8 @@
   export default {
     data: function () {
       return {
-        heros: []
+        my_heros: [],
+
       };
     },
     created: function () {
@@ -12,7 +13,7 @@
         .get("/my_heroes.json")
         .then((response) => {
           console.log("gathering my heroes", response);
-          this.heros = response.data;
+          this.my_heros = response.data;
         })
     },
     methods: {},
@@ -20,47 +21,47 @@
 </script>
 
 <template>
-  <div class="myHeroes">
+  <div class="myHeros">
     <h1>My Heroes</h1>
-    <div v-for="hero in heros" v-bind:key="hero.id">
+    <div v-for="my_hero in my_heros" v-bind:key="my_hero.id">
       <div>
-        <h2>{{hero.first_power_count}}</h2>
-        <h2>{{hero.second_power_count}}</h2>
-        <h2>{{hero.third_power_count}}</h2>
+        <h2>{{my_hero.first_power_count}}</h2>
+        <h2>{{my_hero.second_power_count}}</h2>
+        <h2>{{my_hero.third_power_count}}</h2>
       </div>
-      <div class="heroesInfo">
-        <img v-bind:src="hero.cover_url" alt="">
-        <img v-bind:src="hero.mpq_art_url" alt="">
-        <h1>{{hero.name}} - {{hero.subname}}</h1>
+      <div class="my_heroesInfo">
+        <img v-bind:src="my_hero.hero.cover_url" alt="">
+        <img v-bind:src="my_hero.hero.mpq_art_url" alt="">
+        <h1>{{my_hero.hero.name}} - {{my_hero.hero.subname}}</h1>
         <div class="Game Description">
-          <p>{{hero.game_description}}</p>
+          <p>{{my_hero.hero.game_description}}</p>
         </div>
         <div>
-          <h2>{{hero.stars}} Star</h2>
+          <h2>{{my_hero.hero.stars}} Star</h2>
           <div class="Base Powers">
             <div class="First Base Power">
-              <h3 class="Power Title">{{hero.first_power_name}}: {{hero.first_power_color}} - {{hero.first_power_cost}}</h3>
-              <p class="Power Description">{{hero.first_power_description}}</p>
+              <h3 class="Power Title">{{my_hero.hero.first_power_name}}: {{my_hero.hero.first_power_color}} - {{my_hero.hero.first_power_cost}}</h3>
+              <p class="Power Description">{{my_hero.hero.first_power_description}}</p>
             </div>
             <div class="First Base Power Alt">
-              <h3 class="Power Title">{{hero.first_power_name_alt}}: {{hero.first_power_color_alt}} - {{hero.first_power_cost_alt}}</h3>
-              <p class="Power Description">{{hero.first_power_description_alt}}</p>
+              <h3 class="Power Title">{{my_hero.hero.first_power_name_alt}}: {{my_hero.hero.first_power_color_alt}} - {{my_hero.hero.first_power_cost_alt}}</h3>
+              <p class="Power Description">{{my_hero.hero.first_power_description_alt}}</p>
             </div>
             <div class="Second Base Power">
-              <h3 class="Power Title">{{hero.second_power_name}}: {{hero.second_power_color}} - {{hero.second_power_cost}}</h3>
-              <p class="Power Description">{{hero.second_power_description}}</p>
+              <h3 class="Power Title">{{my_hero.hero.second_power_name}}: {{my_hero.hero.second_power_color}} - {{my_hero.hero.second_power_cost}}</h3>
+              <p class="Power Description">{{my_hero.hero.second_power_description}}</p>
             </div>
             <div class="Second Base Power Alt">
-              <h3 class="Power Title">{{hero.second_power_name_alt}}: {{hero.second_power_color_alt}} - {{hero.second_power_cost_alt}}</h3>
-              <p class="Power Description">{{hero.second_power_description_alt}}</p>
+              <h3 class="Power Title">{{my_hero.hero.second_power_name_alt}}: {{my_hero.hero.second_power_color_alt}} - {{my_hero.hero.second_power_cost_alt}}</h3>
+              <p class="Power Description">{{my_hero.hero.second_power_description_alt}}</p>
             </div>
             <div class="Third Base Power">
-              <h3 class="Power Title">{{hero.third_power_name}}: {{hero.third_power_color}} - {{hero.third_power_cost}}</h3>
-              <p class="Power Description">{{hero.third_power_description}}</p>
+              <h3 class="Power Title">{{my_hero.hero.third_power_name}}: {{my_hero.hero.third_power_color}} - {{my_hero.hero.third_power_cost}}</h3>
+              <p class="Power Description">{{my_hero.hero.third_power_description}}</p>
             </div>
             <div class="Third Base Power Alt">
-              <h3 class="Power Title">{{hero.third_power_name_alt}}: {{hero.third_power_color_alt}} - {{hero.third_power_cost_alt}}</h3>
-              <p class="Power Description">{{hero.third_power_description_alt}}</p>
+              <h3 class="Power Title">{{my_hero.hero.third_power_name_alt}}: {{my_hero.hero.third_power_color_alt}} - {{my_hero.hero.third_power_cost_alt}}</h3>
+              <p class="Power Description">{{my_hero.hero.third_power_description_alt}}</p>
             </div>
           </div>
         </div>
