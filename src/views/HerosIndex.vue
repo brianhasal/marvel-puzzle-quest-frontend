d9836366f5bdb96570851dd5ea8382b4a4f5cd37
Brian Hasal <brian.hasal@gmail.com>
2022-07-11 04:48:38 -0700

    Modal for Heros Index success, still entering borders

--- src/views/HerosIndex.vue
+++ src/views/HerosIndex.vue
@@ -42,12 +42,57 @@
               <img v-if="hero.hover" class="card-img-top" :src="`${hero.mpq_art_url}`" alt="">
               <img v-else class="card-img-top" :src="`${hero.cover_url}`" alt="">
             </div>
+
+            <!-- ############################ -->
+
+            <div class="modal fade" :id="`hero${hero.id}`" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-scrollable">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">{{hero.name}} - {{hero.subname}}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    <div class="row">
+                      <span class="border border-2">
+
+                        <div class="col-3">{{hero.first_power_color}}: {{hero.first_power_cost}}</div>
+                      </span>
+                      <span class="border border-2">
+
+                        <div class="col-9">{{hero.first_power_name}}: {{hero.first_power_description}}</div>
+                      </span>
+                    </div>
+                    <div class="row">
+                      <div class="col-3">{{hero.second_power_color}}: {{hero.second_power_cost}}</div>
+                      <div class="col-9">{{hero.second_power_name}}: {{hero.second_power_description}}</div>
+                    </div>
+                    <div class="row">
+                      <div class="col-3">{{hero.third_power_color}}: {{hero.third_power_cost}}</div>
+                      <div class="col-9">{{hero.third_power_name}}: {{hero.third_power_description}}</div>
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+              <!-- ############################ -->
+              
             <div class="card-body">
               <h5 class="card-title">{{hero.name}} <h6>{{hero.stars}} Stars</h6></h5> 
               <!-- if subname exists, add subname here -->
 
               <button>Add to Roster</button>
-              <button>Stats</button>
+              <p>{{hero.modal}}</p>
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" :data-bs-target="`#hero${hero.id}`">Powers</button>
+
+
+
+              <!-- <button v-on:click="hero.modal = !hero.modal">Stats</button> -->
+              <br>
               <a v-bind:href="`/heroes/${hero.id}.json`">Click for More</a>
               <!-- <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> -->
             </div>
