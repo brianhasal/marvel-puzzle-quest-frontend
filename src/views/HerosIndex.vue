b8872954c21d847fe891738611cbe4a19d580743
Brian Hasal <brian.hasal@gmail.com>
2022-07-11 06:59:34 -0700

    Added better grid borders to Hero Index modals

--- src/views/HerosIndex.vue
+++ src/views/HerosIndex.vue
@@ -53,27 +53,56 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
-                    <div class="row">
-                      <span class="border border-2">
+                    <div class="row g-0 gy-0">
+                        <div class="col-md-3">
+                          <div class="modalBorders">
+                            {{hero.first_power_color}}: {{hero.first_power_cost}}
+                          </div>
+                        </div>
 
-                        <div class="col-3">{{hero.first_power_color}}: {{hero.first_power_cost}}</div>
-                      </span>
-                      <span class="border border-2">
 
-                        <div class="col-9">{{hero.first_power_name}}: {{hero.first_power_description}}</div>
-                      </span>
+                        <div class="col-md-9">
+                          <div class="modalBorders">
+                            {{hero.first_power_name}}: {{hero.first_power_description}}
+                          </div>
+                        </div>
+
+
                     </div>
-                    <div class="row">
-                      <div class="col-3">{{hero.second_power_color}}: {{hero.second_power_cost}}</div>
-                      <div class="col-9">{{hero.second_power_name}}: {{hero.second_power_description}}</div>
+                    <div class="row g-0 gy-0">
+                      <div class="col-3">
+                        <div class="modalBorders">
+                          {{hero.second_power_color}}: {{hero.second_power_cost}}
+                        </div>
+                      </div>
+
+                      <div class="col-9">
+                        <div class="modalBorders">
+                          {{hero.second_power_name}}: {{hero.second_power_description}}
+                        </div>
+                      </div> 
                     </div>
-                    <div class="row">
-                      <div class="col-3">{{hero.third_power_color}}: {{hero.third_power_cost}}</div>
-                      <div class="col-9">{{hero.third_power_name}}: {{hero.third_power_description}}</div>
+
+
+                    <div class="row g-0 gy-0">
+                      <div class="col-3">
+                        <div class="modalBorders">
+                          {{hero.third_power_color}}: {{hero.third_power_cost}}
+                        </div>
+                      </div>
+
+                      <div class="col-9">
+                        <div class="modalBorders">
+                          {{hero.third_power_name}}: {{hero.third_power_description}}
+                        </div>
+                      </div>
+
                     </div>
                   </div>
                   <div class="modal-footer">
+                    <a v-bind:href="`/heroes/${hero.id}.json`">Click for More</a>
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    
                   </div>
                 </div>
               </div>
@@ -85,46 +114,19 @@
               <h5 class="card-title">{{hero.name}} <h6>{{hero.stars}} Stars</h6></h5> 
               <!-- if subname exists, add subname here -->
 
-              <button>Add to Roster</button>
-              <p>{{hero.modal}}</p>
+              <button class="btn btn-primary">Add to Roster</button>
               <button type="button" class="btn btn-primary" data-bs-toggle="modal" :data-bs-target="`#hero${hero.id}`">Powers</button>
 
 
 
               <!-- <button v-on:click="hero.modal = !hero.modal">Stats</button> -->
               <br>
-              <a v-bind:href="`/heroes/${hero.id}.json`">Click for More</a>
+
               <!-- <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> -->
             </div>
           </div>
         </div>
-        <!-- <div class="col">
-          <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div> -->
+
       </div>
       <!-- <div>
         <img v-bind:src="hero.cover_url" alt="">
@@ -177,5 +179,18 @@
   .card {
     max-width: 202px;
     
+  }
+
+  .modalBorders {
+
+    border: black thin solid;
+    /* border-width: auto; */
+    padding: 2px;
+    padding-left: 4px;
+    
+    
+    
+    
+
   }
 </style>
