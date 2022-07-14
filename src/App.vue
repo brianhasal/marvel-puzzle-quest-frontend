72ef6182d290024976a0940d2516f57f094cc561
Brian Hasal <brian.hasal@gmail.com>
2022-07-14 06:46:04 -0700

    Configure Netlify Redirects

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
+
+
+
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container px-5">
@@ -5,6 +8,7 @@
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                  <!-- <li class="nav-item"><a class="nav-link" href="/">Game Sample Videos</a></li> -->
                   <li class="nav-item"><a class="nav-link" href="/heroes">All Heroes</a></li>
                   <li class="nav-item"><a class="nav-link" href="/my_heroes">My Roster</a></li>
 
@@ -63,7 +67,9 @@
 
                   </div>
               </div>
-              <!-- <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center"><img class="img-fluid rounded-3 my-5" src="https://res.cloudinary.com/dvhdgjued/image/upload/v1657694757/capstone_images/miniheader_hppnfn.jpg" alt="..." /></div> -->
+              <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center">
+                <!-- <img class="img-fluid rounded-3 my-5" src="https://res.cloudinary.com/dvhdgjued/image/upload/v1657694757/capstone_images/miniheader_hppnfn.jpg" alt="" /> -->
+              </div>
           </div>
       </div>
     </div>
